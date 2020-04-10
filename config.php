75c2e2f8a1e38fb739f2e4d3e56af2368c4f756d
davidm5375 <dovidm5375@gmail.com>
2020-04-10 04:44:23 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-$mysql_host = '';
+$mysql_host = '	198.23.199.194';
 $mysql_port = '3306';
-$mysql_user = '';
-$mysql_password = '';
-$mysql_database = 'chatlog';
+$mysql_user = '	mc59696';
+$mysql_password = 'b5caec2e82';
+$mysql_database = 'mc59696';
 
-$network_name = '';
+$network_name = 'MobSpot';
 
 
 ?>
