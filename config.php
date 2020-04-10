90111ce537b5ca299773c9860d31c69dac4a8095
davidm5375 <dovidm5375@gmail.com>
2020-04-10 04:50:41 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$mysql_host = '	198.23.199.194';
+$mysql_host = '198.23.199.194';
 $mysql_port = '3306';
-$mysql_user = '	mc59696';
+$mysql_user = 'mc59696';
 $mysql_password = 'b5caec2e82';
 $mysql_database = 'mc59696';
 
